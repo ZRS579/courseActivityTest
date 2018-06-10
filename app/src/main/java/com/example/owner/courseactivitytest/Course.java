737335eb7ab096ffe23ef87da01092dcc1ab80c1
Owner <j94679@gmail.com>
2018-06-10 17:52:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/owner/courseactivitytest/Course.java
@@ -0,0 +1,4 @@
+package com.example.owner.courseactivitytest;
+
+public class Course {
+}
---
+++ app/src/main/java/com/example/owner/courseactivitytest/testTest.java
@@ -0,0 +1,16 @@
+package com.example.owner.courseactivitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class testTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_test);
+
+        Button test = (Button)findViewById(R.id.button2);
+    }
+}
